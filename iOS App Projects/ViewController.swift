34395314062cc44ba8d9b94ade7aeff4935e0f3a
sivilabdullah <194308002@ogr.ebyu.edu.tr>
2022-10-28 12:03:31 +0300

    Initial Commit

--- iOS App Projects/ViewController.swift
+++ iOS App Projects/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var PasswdTextField: UITextField!
+    @IBOutlet weak var UsrnmeTextField: UITextField!
+    @IBOutlet weak var LoadinfoActivtyIndicator: UIActivityIndicatorView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func LogInBtnAct(_ sender: UIButton) {
+        print(" Hi \(UsrnmeTextField.text ?? "Please Enter a valid username")")
+        print (" Your Password : \(PasswdTextField.text ?? "incorrect email username or password.") ..is secure ")
+        LoadinfoActivtyIndicator.startAnimating()
+    }
+    
 }
 
