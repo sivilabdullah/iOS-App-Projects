3611115aa4bb5f79c6a607e59983175023b400c8
sivilabdullah <194308002@ogr.ebyu.edu.tr>
2022-10-28 10:09:58 +0300

    Initial Commit

---
+++ iOS App Projects/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS App Projects
+//
+//  Created by abdullah's Monterey  on 28.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
